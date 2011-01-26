66d0a42bfe78602a8f98dd95801fa6da7d26f0a9
Amit Kumar <toamitkumar@gmail.com>
2011-01-26 15:11:03 -0500

    first commit

---
+++ app/controllers/queues_controller.rb
@@ -0,0 +1,5 @@
+class QueuesController < ApplicationController
+  def create
+    
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ResqueApp::Application.config.secret_token = '50dfe017084ec5aa6dfd75958f39317b40b9d6158d641e75b3b46a46b351d2b3303ae3492df1429e01e6d98d1ee4d97846d860c425e355cee4af263cc4c3036c'
