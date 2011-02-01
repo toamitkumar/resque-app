feae16bfec842b16f8ba6c4c17013f7d4f4d3681
Amit Kumar <toamitkumar@Amit-Kumars-MacBook-Pro.local>
2011-01-31 23:42:43 -0500

    removed client name

--- app/controllers/queues_controller.rb
+++ app/controllers/queues_controller.rb
@@ -1,6 +1,6 @@
 class QueuesController < ApplicationController
   def create
-    options = {:body => params[:data], :from => "sender@mckinsey.com"}
+    options = {:body => params[:data], :from => "sender@test.com"}
     Resque.enqueue(QueueClient, options)
     redirect_to(root_url, :notice => "Successfully enqueued !")
   end
